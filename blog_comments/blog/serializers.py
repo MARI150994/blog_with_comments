5b2e89aa1fb2428f69f316ba64bbd23789ad9b63
MARI150994 <mari.utkina.94@mail.ru>
2022-04-24 17:00:20 +0300

    add detail article ser anv views

--- blog_comments/blog/serializers.py
+++ blog_comments/blog/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Article, Comment
 
+
 class ArticleListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['name', 'description']
+
+
+class ArticleDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ['name', 'description']
--- blog_comments/blog/views.py
+++ blog_comments/blog/views.py
@@ -1,7 +1,13 @@
 from rest_framework import generics
 from .models import Article, Comment
+from .serializers import ArticleListSerializer, ArticleDetailSerializer
 
 
 class ArticleList(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleListSerializer
+
+
+class ArticleDetail(generics.RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleDetailSerializer
